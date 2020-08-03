4003d050fff5ae6c113278397ea0edde0686b385
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-08-03 16:07:25 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@
   var name  = prompt("Qual è il tuo nome?");
   var surname = prompt("Qual è il tuo cognome?");
   var color = prompt("qual è il tuo colore preferito?")
-  var number = 19;
+  var number = parseInt(19);
   // var number = Math.floor((Math.random() * 100) + 1);
   // var password = name + surname + color + number;  For random number
-  var password = name + surname + color + number.toString();
+  var password = name + surname + color + number;
 
 // Password
 
